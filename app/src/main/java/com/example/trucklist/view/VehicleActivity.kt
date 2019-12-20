93cbef51da8416b4619c81d7eabaae108df98130
hedie rocha <helder.rocha@agileprocess.com.br>
2019-12-20 16:08:25 -0300

    vehicle adapter

--- app/src/main/java/com/example/trucklist/view/VehicleActivity.kt
+++ app/src/main/java/com/example/trucklist/view/VehicleActivity.kt
@@ -1,18 +1,26 @@
 package com.example.trucklist.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.trucklist.R
 import com.example.trucklist.ViewModelFactory
 import com.example.trucklist.model.Vehicle
+import com.example.trucklist.view.adapter.VehicleAdapter
 import com.example.trucklist.view_model.VehicleViewModel
 import dagger.android.AndroidInjection
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class VehicleActivity : AppCompatActivity() {
+    lateinit var adapter: VehicleAdapter
+
     @Inject
     lateinit var vehiclesVMFactory: ViewModelFactory<VehicleViewModel>
 
@@ -36,5 +44,19 @@ class VehicleActivity : AppCompatActivity() {
         list?.forEach {
             Log.i("Helder", it.plate)
         }
+        list?.let { updateAdapter(it) }
+    }
+
+    private fun updateAdapter(list: List<Vehicle>) {
+        rc_trucks.layoutManager = LinearLayoutManager(this)
+        rc_trucks.setHasFixedSize(true)
+        adapter = VehicleAdapter({ vehicle: String -> partItemClicked(vehicle) } )
+        adapter.update(list)
+        rc_trucks.adapter = adapter
+
+    }
+
+    private fun partItemClicked(vehicle: String) {
+        Toast.makeText(this, vehicle, Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/example/trucklist/view/adapter/VehicleAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.trucklist.view.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.trucklist.R
+import com.example.trucklist.model.Vehicle
+import kotlinx.android.synthetic.main.item_plate.view.*
+
+
+class VehicleAdapter(val clickListener: ((String) -> Unit)?) : RecyclerView.Adapter<VehicleAdapter.ViewHolder>() {
+    var vehicles: List<Vehicle>
+
+    init {
+        vehicles = listOf()
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(vehicleItem: Vehicle, clickListener: ((String) -> Unit)?) {
+            itemView.tv_title.text = vehicleItem.plate
+            itemView.setOnClickListener { clickListener?.let { it1 -> it1(vehicleItem.plate) } }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_plate, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = this.vehicles.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int){
+        (holder as ViewHolder).bind(this.vehicles[position], clickListener)
+    }
+
+    fun updateList() {
+        notifyDataSetChanged()
+    }
+
+    fun update(vehicles: List<Vehicle>) {
+        this.vehicles = emptyList()
+        this.vehicles = vehicles
+        updateList()
+    }
+}
